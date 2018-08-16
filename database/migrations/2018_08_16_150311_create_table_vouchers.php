3059d56f92d7df9488827e526c9141926cc988c7
Liman Adamu Labaran <glimanadamu@gmail.com>
2018-08-16 15:14:55 +0000

    Adds migration files

---
+++ database/migrations/2018_08_16_150311_create_table_vouchers.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableVouchers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code')->unique();
+            $table->integer('offer_id')->unsigned();
+            $table->integer('recipient_id')->unsigned()->nullable()->default(null);
+            $table->dateTime('expires_at')->nullable()->default(null);
+            $table->integer('is_used')->unsigned();
+            $table->dateTime('used_at')->nullable()->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vouchers');
+    }
+}
